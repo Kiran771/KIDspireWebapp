1e53629350f68e2eef5316112deb314bee3a8cdd
Kiran771 <kiransaud51@gmail.com>
2025-04-09 08:33:22 +0545

    added error boolean for service

--- java/com/kidspire/service/RegisterService.java
+++ java/com/kidspire/service/RegisterService.java
@@ -1,17 +1,20 @@
 package com.kidspire.service;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.kidspire.config.DbConfig;
 
 public class RegisterService {
 	private Connection dbconn;
+	private Boolean hasConnectionError = false;
 	
 	public RegisterService(){
 		try {
 			this.dbconn=DbConfig.getDbConnection();
 			
 		}catch(SQLException|ClassNotFound ex){
+			hasConnectionError = true;
 			System.err.println("Database connection error:"+ ex.getMessage());
 			ex.printStackTrace();
 		}
